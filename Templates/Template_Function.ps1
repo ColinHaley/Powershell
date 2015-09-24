fe3975b27606ad01f1a8fd0be4ce4d0c9c92f605
Colin Haley <admin@colinhaley.net>
2015-09-24 10:25:53 -0400

    add base function template

---
+++ Templates/Template_Function.ps1
@@ -0,0 +1,20 @@
+Function Do-Work{
+<#
+	.SYNOPSIS
+		Quick note on what Do-Work does.
+	
+	.DESCRIPTION
+		More in depth explaination of Do-Work.
+
+	.PARAMETER ComputerName
+		ComputerName will be a parameter in about 95% of my scripts, describe it here.
+		Default value = 'localhost'
+
+	.EXAMPLE
+		Do-Work -ComputerName ThisComputer
+#>
+
+
+
+
+}#Function
